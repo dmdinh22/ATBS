b0c1b3eac3b999c5b3e753dff793cd6dd3b15aca
David <dmdinh22@gmail.com>
2018-02-15 00:41:42 +0000

    today's exercises

---
+++ Ch6/picnic_table.py
@@ -0,0 +1,7 @@
+def print_picnic(itemsDict, leftWidth, rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+picnic_items = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+print_picnic(picnic_items, 12, 5)
+print_picnic(picnic_items, 20, 6)
