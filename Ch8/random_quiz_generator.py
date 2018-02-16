3ddd331757cc434faa5f27997b178f8a39e3b5d2
David <dmdinh22@gmail.com>
2018-02-16 18:26:43 +0000

    Ch8 completed

--- Ch8/random_quiz_generator.py
+++ Ch8/random_quiz_generator.py
@@ -13,14 +13,12 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
    'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
    'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
    'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
-   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New
-   Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
    'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
    'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
    'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
    'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
-   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West
-   Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
 # generate 35 quiz files
 for quiz_num in range(35):
@@ -36,6 +34,25 @@ for quiz_num in range(35):
     states = list(capitals.keys())
     random.shuffle(states)
 
-    #TODO: loop through all 50 states, making a question for each
+    #loop through all 50 states, making a question for each
+    for question_num in range(50):
+        # get right and wrong answers
+        correct_answer = capitals[states[question_num]]
+        wrong_answers = list(capitals.values())
+        del wrong_answers[wrong_answers.index(correct_answer)]
+        wrong_answers = random.sample(wrong_answers, 3)
+        answer_options = wrong_answers + [correct_answer]
+        random.shuffle(answer_options)
+
+        #write the questions and answer options to the quiz file
+        quiz_file.write('%s. What is the capital of %s?\n' % (question_num + 1,
+               states[question_num]))
+        for i in range(4):
+            quiz_file.write(' %s. %s\n' % ('ABCD'[i], answer_options[i]))
+            quiz_file.write('\n')
+        #write the answer key to a file
+        answer_key_file.write('%s. %s\n' % (question_num + 1, 'ABCD'[answer_options.index(correct_answer)]))
+    quiz_file.close()
+    answer_key_file.close()
 
     
